3515607d349413f570af9d5ddedbb306e9ca4731
linkuello <144430394+linkuello@users.noreply.github.com>
2024-11-26 00:48:07 +0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_OMSUAd6QF', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '13i2gcc71grich6dr3a7pt52q2', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://cognito-idp.us-east-2.amazonaws.com/us-east-2_OMSUAd6QF/.well-known/jwks.json' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
